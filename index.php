c0812268377312ca7fe6c8649307159986f31a5f
EnrikasNemc <33262515+EnrikasNemc@users.noreply.github.com>
2017-11-13 01:29:38 +0200

    Array n1

--- index.php
+++ index.php
@@ -1,1 +1,216 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Car Crashes</title>
+</head>
+<body>
+
+</body>
+<script type="text/javascript"> 
+/*
+var Carcrash = {
+	"crash":{
+		"2017-11-14":
+		[
+			{"kaunoAp":{
+					"Dalyvis":{
+						"Masina":[30],
+						"Dviratis":[2],
+						"Pestysis":[8],
+					}
+				}
+			},
+			{"siauliuAP":{
+					"Dalyvis":{
+						"Masina":[4],
+						"Dviratis":[9],
+						"Pestysis":[5]
+					}
+				}
+			},
+			{"VilniausAP":{
+					"Dalyvis":{
+						"Masina":[3],
+						"Dviratis":[8],
+						"Pestysis":[2],
+					}
+				}
+			},
+		],
+		"2017-11-15":
+		[
+			{"kaunoAp":{
+					"Dalyvis":{
+						"Masina":[45],
+						"Dviratis":[0],
+						"Pestysis":[2],
+					}
+				}
+			},
+			{"siauliuAP":{
+					"Dalyvis":{
+						"Masina":[5],
+						"Dviratis":[2],
+						"Pestysis":[4]
+					}
+				}
+			},
+			{"VilniausAP":{
+					"Dalyvis":{
+						"Masina":[80],
+						"Dviratis":[0],
+						"Pestysis":[4],
+					}
+				}
+			},
+		],
+		"2017-11-16":
+		[
+			{"kaunoAp":{
+					"Dalyvis":{
+						"Masina":[0],
+						"Dviratis":[5],
+						"Pestysis":[8],
+					}
+				}
+			},
+			{"siauliuAP":{
+					"Dalyvis":{
+						"Masina":[6],
+						"Dviratis":[2],
+						"Pestysis":[1]
+					}
+				}
+			},
+			{"VilniausAP":{
+					"Dalyvis":{
+						"Masina":[7],
+						"Dviratis":[9],
+						"Pestysis":[2],
+					}
+				}
+			},
+		],
+		"2017-11-17":
+		[
+			{"kaunoAp":{
+					"Dalyvis":{
+						"Masina":[50],
+						"Dviratis":[4],
+						"Pestysis":[10],
+					}
+				}
+			},
+			{"siauliuAP":{
+					"Dalyvis":{
+						"Masina":[3],
+						"Dviratis":[0],
+						"Pestysis":[1]
+					}
+				}
+			},
+			{"VilniausAP":{
+					"Dalyvis":{
+						"Masina":[60],
+						"Dviratis":[5],
+						"Pestysis":[7],
+					}
+				}
+			},
+		],
+		"2017-11-18":
+		[
+			{"kaunoAp":{
+					"Dalyvis":{
+						"Masina":[8],
+						"Dviratis":[20],
+						"Pestysis":[84],
+					}
+				}
+			},
+			{"siauliuAP":{
+					"Dalyvis":{
+						"Masina":[30],
+						"Dviratis":[2],
+						"Pestysis":[5]
+					}
+				}
+			},
+			{"VilniausAP":{
+					"Dalyvis":{
+						"Masina":[57],
+						"Dviratis":[6],
+						"Pestysis":[4],
+					}
+				}
+			},
+		],
+		"2017-11-19":
+		[
+			{"kaunoAp":{
+					"Dalyvis":{
+						"Masina":[96],
+						"Dviratis":[71],
+						"Pestysis":[4],
+					}
+				}
+			},
+			{"siauliuAP":{
+					"Dalyvis":{
+						"Masina":[8],
+						"Dviratis":[1],
+						"Pestysis":[20]
+					}
+				}
+			},
+			{"VilniausAP":{
+					"Dalyvis":{
+						"Masina":[10],
+						"Dviratis":[0],
+						"Pestysis":[1],
+					}
+				}
+			},
+		],
+		"2017-11-20":
+		[
+			{"kaunoAp":{
+					"Dalyvis":{
+						"Masina":[64],
+						"Dviratis":[8],
+						"Pestysis":[1],
+					}
+				}
+			},
+			{"siauliuAP":{
+					"Dalyvis":{
+						"Masina":[24],
+						"Dviratis":[2],
+						"Pestysis":[2]
+					}
+				}
+			},
+			{"VilniausAP":{
+					"Dalyvis":{
+						"Masina":[45],
+						"Dviratis":[1],
+						"Pestysis":[1],
+					}
+				}
+			},
+		]
+	}
+} 
+console.log(Carcrash);
+*/
+
+
+
+
+
+
+
+
+ </script>
+</html>
+
